538923afeca826a15e4b2c830e4fb58e4532360c
powroupi <powroupi@users.noreply.github.com>
2015-09-01 17:00:26 +0800

    Easier to add/remove Rigid Bodies and Joints.

--- mmd_tools/operators/rigid_body.py
+++ mmd_tools/operators/rigid_body.py
@@ -46,18 +46,19 @@ class AddRigidBody(Operator):
         loc = (0.0, 0.0, 0.0)
         rot = (0.0, 0.0, 0.0)
         bone = None
-        if context.active_pose_bone is not None:
-            loc = context.active_pose_bone.location
-            bone = context.active_pose_bone.name
-        elif context.active_bone is not None:
-            loc = context.active_bone.head
-            bone = context.active_bone.name
-        elif arm is not None and len(arm.pose.bones) > 0:
-            loc = arm.pose.bones[0].location
-            bone = arm.pose.bones[0].name
-            
         bpy.ops.object.mode_set(mode='OBJECT')
-                    
+        target_bone = context.active_bone
+        if target_bone:
+            if type(target_bone) is not bpy.types.Bone:
+                target_bone = obj.data.bones[target_bone.name]
+        elif arm is not None and len(arm.data.bones) > 0:
+            target_bone = arm.data.bones[0]
+
+        if target_bone: # bpy.types.Bone
+            loc = (target_bone.head_local+target_bone.tail_local)/2
+            rot = (target_bone.tail_local-target_bone.head_local).to_track_quat('Z','Y').to_euler()
+            bone = target_bone.name
+
         rigid = rig.createRigidBody(
                 name = self.name_j,
                 name_e = self.name_e,
@@ -76,13 +77,10 @@ class AddRigidBody(Operator):
                 bounce = 0.5,
                 bone = bone,
                 )
-        if mmd_root.show_rigid_bodies:
-            rigid.hide = False
-            utils.selectAObject(rigid)
-        else:
-            rigid.hide = True
-            utils.selectAObject(obj)
-            
+        if not mmd_root.show_rigid_bodies:
+            mmd_root.show_rigid_bodies = True
+        utils.selectAObject(rigid)
+
         if 'mmd_tools.'+mmd_root.name+'_all' in bpy.data.groups.keys(): # Add Rigid to allObjectsGroup
             bpy.data.groups['mmd_tools.'+mmd_root.name+'_all'].objects.link(rigid)
         if 'mmd_tools.'+mmd_root.name+'_rigids' in bpy.data.groups.keys(): # Add Rigid to RigidsGroup
@@ -91,6 +89,26 @@ class AddRigidBody(Operator):
         return { 'FINISHED' }
         
     def invoke(self, context, event):
+        bone = context.active_pose_bone or context.active_bone
+        if type(bone) is not bpy.types.PoseBone:
+            root = mmd_model.Model.findRoot(context.active_object)
+            arm = mmd_model.Model(root).armature()
+            if arm is not None and len(arm.pose.bones) > 0:
+                if bone is None:
+                    bone = arm.pose.bones[0]
+                elif bone.name in arm.pose.bones:
+                    bone = arm.pose.bones[bone.name]
+            else:
+                self.name_j = 'Rigid'
+                self.name_e = 'Rigid_e'
+        if bone:
+            self.name_j = bone.name
+            if type(bone) is bpy.types.PoseBone:
+                mmd_bone = bone.mmd_bone
+                if len(mmd_bone.name_j) > 0:
+                    self.name_j = mmd_bone.name_j
+                if len(mmd_bone.name_e) > 0:
+                    self.name_e = mmd_bone.name_e
         vm = context.window_manager
         return vm.invoke_props_dialog(self) 
 
@@ -105,8 +123,11 @@ class RemoveRigidBody(Operator):
         if obj.mmd_type != 'RIGID_BODY':
             self.report({ 'ERROR' }, "Select the Rigid Body to be deleted")
             return { 'CANCELLED' }
+        root = mmd_model.Model.findRoot(obj)
         utils.selectAObject(obj) #ensure this is the only one object select
         bpy.ops.object.delete(use_global=True)
+        if root:
+            utils.selectAObject(root)
         return { 'FINISHED' } 
 
 class AddJoint(Operator): 
@@ -119,14 +140,35 @@ class AddJoint(Operator):
         root = mmd_model.Model.findRoot(obj)
         rig = mmd_model.Model(root) 
         mmd_root = rig.rootObject().mmd_root
+        name = 'Joint'
+        name_e = 'Joint_e'
+        loc = (0.0, 0.0, 0.0)
+        rigid_a = None
+        rigid_b = None
+        if mmd_model.isRigidBodyObject(obj):
+            if len(context.selected_objects) == 2:
+                if obj == context.selected_objects[0]:
+                    rigid_a = context.selected_objects[1]
+                else:
+                    rigid_a = context.selected_objects[0]
+            name = obj.name
+            name_e = obj.mmd_rigid.name_e
+            rigid_b = obj
+            relation = rigid_b.constraints['mmd_tools_rigid_parent']
+            arm = relation.target
+            bone_name = relation.subtarget
+            if arm is not None and bone_name in arm.data.bones:
+                loc = arm.data.bones[bone_name].head_local
+            else:
+                loc = rigid_b.location if rigid_a is None else (rigid_a.location+rigid_b.location)/2
         joint = rig.createJoint(
-                name = 'Joint',
-                name_e = 'Joint_e',
-                location = [0, 0, 0],
+                name = name,
+                name_e = name_e,
+                location = loc,
                 rotation = [0, 0, 0],
                 size = 0.5 * mmd_root.scale,
-                rigid_a = None,
-                rigid_b = None,
+                rigid_a = rigid_a,
+                rigid_b = rigid_b,
                 maximum_location = [0, 0, 0],
                 minimum_location = [0, 0, 0],
                 maximum_rotation = [0, 0, 0],
@@ -134,17 +176,14 @@ class AddJoint(Operator):
                 spring_linear = [0, 0, 0],
                 spring_angular = [0, 0, 0],
                 )
-        if mmd_root.show_joints:
-            joint.hide = False
-            utils.selectAObject(joint)
-        else:
-            joint.hide = True
-            utils.selectAObject(obj)
-            
+        if not mmd_root.show_joints:
+            mmd_root.show_joints = True
+        utils.selectAObject(joint)
+
         if 'mmd_tools.'+mmd_root.name+'_all' in bpy.data.groups.keys(): # Add Joint to allGroup
-            bpy.data.groups['mmd_tools.'+mmd_root.name+'_all'].link(joint)
+            bpy.data.groups['mmd_tools.'+mmd_root.name+'_all'].objects.link(joint)
         if 'mmd_tools.'+mmd_root.name+'_joints' in bpy.data.groups.keys(): # Add Joint to joints group
-            bpy.data.groups['mmd_tools.'+mmd_root.name+'_joints'].link(joint)
+            bpy.data.groups['mmd_tools.'+mmd_root.name+'_joints'].objects.link(joint)
         return { 'FINISHED' }
     
 class RemoveJoint(Operator):
@@ -158,6 +197,9 @@ class RemoveJoint(Operator):
         if obj.mmd_type != 'JOINT':
             self.report({ 'ERROR' }, "Select the Joint to be deleted")
             return { 'CANCELLED' }
+        root = mmd_model.Model.findRoot(obj)
         utils.selectAObject(obj) #ensure this is the only one object select
         bpy.ops.object.delete(use_global=True)
+        if root:
+            utils.selectAObject(root)
         return { 'FINISHED' }
